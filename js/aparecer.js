1a9840d4667e7ff46d1d4766cecbac1133adec2d
Enne <EnneFofe10@gmail.com>
2023-08-06 00:11:28 -0300

    Add files via upload

---
+++ js/aparecer.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#conteúdo', {duration: 1000});
+
+sr.reveal('.coisa', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+
+sr.reveal('.especial', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById('menu')
 
 function abrirMenu() {
-    if (areas.style.left = '-50%') {
+    if (areas.style.left = '-70%') {
         areas.style.left = '0'
         areas.style.opacity = '100%'
         menu.style.transform = 'rotate(-360deg)'
@@ -9,7 +9,7 @@ function abrirMenu() {
     }
 }
 function sairDoMenu() {
-    areas.style.left = '-50%'
+    areas.style.left = '-70%'
     areas.style.opacity = '0%'
     menu.style.transform = 'rotate(0deg)'
     menu.style.boxShadow = '0px 0px 0px 0px #a479c0'
