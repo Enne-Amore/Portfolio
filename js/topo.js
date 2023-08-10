c317dac9ee6d6a2da6071e33b255839249065e55
Enne <EnneFofe10@gmail.com>
2023-08-10 16:00:17 -0300

    Add files via upload

--- js/topo.js
+++ js/topo.js
@@ -9,9 +9,12 @@ document.addEventListener('scroll', ocultar)
 function ocultar() {
     if (window.scrollY > 1000) {
         topo.style.display = 'flex'
+    } else if (window.scrollY > 1800) {
+        topo.style.display = 'none'
     } else {
         topo.style.display = 'none'
     }
+    
 }
 
 ocultar()
