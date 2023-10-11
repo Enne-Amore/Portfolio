d0e6d348bf9fe8fcc4691a16080bf4b16ca5b785
Enne <EnneFofe10@gmail.com>
2023-10-11 15:21:02 -0300

    aparecimento do conteúdo
    Ajuste no arquivo aparecer.js justamente para as páginas de vídeos e de imagens.

--- src/js/aparecer.js
+++ src/js/aparecer.js
@@ -6,7 +6,12 @@ sr.reveal('#conteudo', {duration: 100});
 sr.reveal('.parte');
 sr.reveal('.txt');
 sr.reveal('.capas');
-sr.reveal('.video');
-sr.reveal('.iguali');
+sr.reveal('.imagens');
 sr.reveal('.secao');
 sr.reveal('.especial');
+
+// Para telas menores
+if (innerWidth <= 1200) {
+    sr.reveal('.video');
+    sr.reveal('.iguali');
+}
