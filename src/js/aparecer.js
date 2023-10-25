cb3ab7291044650a332836bdf270704187f6efec
”Enne” <EnneFofe10@gmail.com>
2023-10-25 16:49:12 -0300

    Aparecer
    Ajeitando o arquivo de fazer o conteúdo aparecer de parte em parte conforme a rolagem de tela ocorre.

--- src/js/aparecer.js
+++ src/js/aparecer.js
@@ -2,16 +2,16 @@
 window.sr = ScrollReveal({ reset: true });
 
 // Chamando as tags para aparecerem
-sr.reveal('#conteudo', {duration: 100});
 sr.reveal('.parte');
 sr.reveal('.txt');
-sr.reveal('.capas');
-sr.reveal('.imagens');
 sr.reveal('.secao');
 sr.reveal('.especial');
 
-// Para telas menores
+// Especificidade para cada tela
 if (innerWidth <= 1200) {
     sr.reveal('.video');
     sr.reveal('.iguali');
+} else {
+    sr.reveal('.capas');
+    sr.reveal('.imagens');
 }
