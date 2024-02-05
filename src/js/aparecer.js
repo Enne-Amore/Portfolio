2bd723770893ee2efb062003bbffe4981eb13744
”Enne” <EnneFofe10@gmail.com>
2024-02-04 21:44:01 -0300

    Animação
    Melhorando muito a animação dos elementos visuais

--- src/js/aparecer.js
+++ src/js/aparecer.js
@@ -2,17 +2,43 @@
 window.sr = ScrollReveal({ reset: true });
 
 // Chamando as tags para aparecerem
+sr.reveal('.secao', {
+    distance: '15px',
+    duration: 1000
+});
+sr.reveal('.txt', {
+    distance: '15px',
+    duration: 1000
+});
+sr.reveal('.especial', {
+    rotate: { x:0, y:80, z:0 },
+    duration: 1500,
+    distance: '5px'
+});
 sr.reveal('.parte');
-sr.reveal('.txt');
-sr.reveal('.secao');
-sr.reveal('.especial');
-sr.reveal('.sites');
 
-// Especificidade para cada tela
-if (innerWidth <= 1200) {
-    sr.reveal('.video');
-    sr.reveal('.iguali');
-} else {
-    sr.reveal('.capas');
-    sr.reveal('.imagens');
-}
+// Conteúdo dinâmico
+sr.reveal('.sites', {
+    distance: '15px',
+    duration: 1000
+});
+sr.reveal('.iguali', {
+    scale: 0.5,
+    duration: 600,
+    easing: 'ease-out',
+});
+sr.reveal('.video', {
+    scale: 0.5,
+    duration: 600,
+    easing: 'ease-out',
+});
+
+// Rodapé
+sr.reveal('.table', {
+    distance: '10px',
+    duration: 1000
+});
+sr.reveal('.list', {
+    distance: '10px',
+    duration: 1000
+});
